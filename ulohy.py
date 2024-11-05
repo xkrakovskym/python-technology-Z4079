55b506d7eb689aee901a7223f526159f8acc5723
mkrakovsky <michal.krakovsky@blindspot.ai>
2024-11-05 20:11:02 +0100

    dokoncece ulohy

---
+++ ulohy.py
@@ -0,0 +1,19 @@
+# 1. Napíšte skript, ktorý importuje modul math a vypočíta druhú odmocninu zo 64.
+# Výsledok vytlačte na obrazovku. Použite funkciu sqrt. Vypíšte aj výsledok.
+
+import math
+#print(math.sqrt(64))
+
+# 2. Z modulu random importujte iba funkciu randint a použite ju na vygenerovanie
+# náhodného celého čísla medzi 1 a 10. Výsledok vytlačte.
+
+from random import randint
+
+#print(randint(1,10))
+
+# 3. Importujte modul datetime ako dt a použite ho na vytlačenie aktuálneho dátumu a času.
+# Použite funkciu datetime.now()
+
+import datetime as dt
+
+print(dt.datetime.now())
